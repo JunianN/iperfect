c6af3b691fe061fa8d1e470bd28b159d9d2e088f
Rizky Junian Nugroho <rizky.j.n@mail.ugm.ac.id>
2024-07-20 16:12:55 +0700

    feat: report page

---
+++ app/report/page.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import Sidebar from "../components/Sidebar";
+
+export default function Report() {
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <Sidebar />
+            <Box>
+
+            </Box>
+        </Box>
+    )
+}
