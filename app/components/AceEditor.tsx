3c07cc8ce2dffc9efbd9647f90370f5331b196f2
Rizky Junian Nugroho <rizky.j.n@mail.ugm.ac.id>
2024-07-20 16:42:00 +0700

    fix: code editor

--- app/components/AceEditor.tsx
+++ app/components/AceEditor.tsx
@@ -4,6 +4,7 @@ import AceEditor from "react-ace"
 import "ace-builds/src-noconflict/mode-python"
 import "ace-builds/src-noconflict/theme-github_dark"
 import "ace-builds/src-noconflict/ext-language_tools"
+import "ace-builds/src-noconflict/snippets/python"
 
 interface CodeEditorProps {
     onChange: (newCode: string) => void;
